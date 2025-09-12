c07c6466a55cf54965f38b38eb036b95a42cc5db
oatsu <oatsu.dev@gmail.com>
2025-09-13 00:48:29 +0900

    util.py に denoise_spike() と easy_interpolate() を追加

--- src/util.py
+++ src/util.py
@@ -6,6 +6,7 @@ Utility functions for audio processing
 import logging
 import sys
 from pathlib import Path
+from warnings import warn
 
 import colored_traceback.auto  # noqa: F401
 import numpy as np
@@ -33,6 +34,61 @@ def setup_logger() -> logging.Logger:
         level=logging.INFO,
     )
     return logging.getLogger(__name__)
+
+
+def easy_interpolate(y: list[float] | np.ndarray) -> float:
+    """スパイクノイズ除去のため、線形補間またはキュービック補間で x=0 の値を求める。
+    y の長さが 2 のときは x1, x2 = (-1, +1) として x=0 の値を求める。
+    y の長さが 4 のときは x1, x2, x3, x4 = (-2, -1, +1, +2) として x=0 の値を求める。
+
+    Args:
+        y1 (float): 1つ目のデータ
+        y2 (float): 2つ目のデータ
+        y3 (float): 3つ目のデータ
+        y4 (float): 4つ目のデータ
+
+    Returns:
+        float: x=0 のときの y の値
+    """
+    # 補間に使う重みを決定する。y の長さが2の時は線形補間、y の長さが4の時はキュービック補間。
+    weights = [1, 1] if len(y) == 2 else [-1, 4, 4, -1] if len(y) == 4 else None
+    if weights is None:
+        msg = 'y must be a list of length 2 or 4.'
+        raise ValueError(msg)
+    return sum([a * b for a, b in zip(y, weights, strict=True)]) / sum(weights)
+
+
+def denoise_spike(f0: np.ndarray) -> np.ndarray:
+    """1次元配列の f0 のスパイクノイズを除去する。
+
+    Args:
+        f0 (np.ndarray): スパイクノイズを除去する対象の f0 配列
+
+    Returns:
+        np.ndarray: スパイクノイズを除去した f0 配列
+    """
+    f0_clean = f0.copy()
+    window = 5
+    half_window = window // 2
+
+    for i in range(half_window, len(f0_clean) - half_window):
+        window_data = f0_clean[i - half_window : i + half_window + 1]
+        q1 = np.quantile(window_data, 0.25)
+        a3 = np.quantile(window_data, 0.75)
+        iqr = a3 - q1
+        lower_bound = q1 - 1.5 * iqr
+        upper_bound = a3 + 1.5 * iqr
+        # スパイクノイズを除去
+        if f0_clean[i] < lower_bound or f0_clean[i] > upper_bound:
+            warn(f'Spike noise detected at index {i}: {f0_clean[i]}', stacklevel=2)
+            y = [
+                f0_clean[i - 2],
+                f0_clean[i - 1],
+                f0_clean[i + 1],
+                f0_clean[i + 2],
+            ]
+            f0_clean[i] = easy_interpolate(y)
+    return f0_clean
 
 
 def overlap_world_feature(
@@ -93,6 +149,8 @@ def crossfade_world_feature(
 
     # クロスフェードなしで合算 (エンベロープ反映後の sp, ap で使う想定)
     if shape is None:
+        fade_out = np.ones((overlap_samples, 1))  # 不要だが後続バグ予防のためダミー
+        fade_in = np.ones((overlap_samples, 1))  # 不要だが後続バグ予防のためダミー
         overlap_area = feature_a[-overlap_samples:] + feature_b[:overlap_samples]
     # 線形クロスフェード (f0で使う想定)
     elif shape == 'linear':
