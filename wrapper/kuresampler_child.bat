170c07279b471a0b14e8f2ea194bb6f7d7df0033
oatsu <oatsu.dev@gmail.com>
2025-09-14 11:47:40 +0900

    Update kuresampler_child.bat

--- wrapper/kuresampler_child.bat
+++ wrapper/kuresampler_child.bat
@@ -2,6 +2,9 @@
 echo ============================== kuresampler ==================================
 echo %0
 echo %*
-echo kuresampler successfully launched!
+set selfdir=%~dp0
+set python_exe=python
+set python_script=%selfdir%..\src\resampler.py
+set model_dir="%selfdir%..\\models\\usfGAN_EnunuKodoku_0826\\"
+%python_exe% "%python_script%" "%*" --model_dir %model_dir%
 echo =============================================================================
-TIMEOUT /T 2 /NOBREAK
