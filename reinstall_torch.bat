790386e7634257974e919f3c2be60aff366cf8a9
oatsu <oatsu.dev@gmail.com>
2025-09-20 01:36:05 +0900

    Update reinstall_torch.bat

--- reinstall_torch.bat
+++ reinstall_torch.bat
@@ -1,3 +1,4 @@
 python-3.12.10-embed-amd64\python.exe -m pip uninstall torch torchaudio torchvision -y
 python-3.12.10-embed-amd64\python.exe -m pip install --upgrade light-the-torch
-python-3.12.10-embed-amd64\python.exe -m light-the-torch install torch torchaudio torchvision
+python-3.12.10-embed-amd64\python.exe -m light_the_torch install torch torchaudio torchvision
+PAUSE
