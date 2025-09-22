5e1d377d003d3825c521bf81c71d3b32c075d913
oatsu <oatsu.dev@gmail.com>
2025-09-23 02:31:38 +0900

    Update kuresampler_child.bat

--- kuresampler_child.bat
+++ kuresampler_child.bat
@@ -8,5 +8,5 @@ set python_exe=%selfdir%python-3.12.10-embed-amd64\python.exe
 set python_script=%selfdir%resampler.py
 @REM set model_dir=%selfdir%models\usfGAN_EnunuKodoku_0826\
 set model_dir=%selfdir%models\usfGAN_Namineritsu_4130\
-%python_exe% %python_script% %* --model_dir %model_dir%
+%python_exe% %python_script% %* --model_dir %model_dir% --use_vocoder_model
 echo =============================================================================
