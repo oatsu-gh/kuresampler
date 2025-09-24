7da8b71881e7541ee79a9c1e3cf005f803db9109
oatsu <oatsu.dev@gmail.com>
2025-09-25 02:42:04 +0900

    Update kuresampler_child.bat

--- kuresampler_child.bat
+++ kuresampler_child.bat
@@ -3,10 +3,11 @@ echo ============================== kuresampler ================================
 @REM echo %0
 echo %*
 set selfdir=%~dp0
-@REM set python_exe=python.exe
-set python_exe=%selfdir%python-3.12.10-embed-amd64\python.exe
+set python_exe=python.exe
+@REM set python_exe=%selfdir%python-3.12.10-embed-amd64\python.exe
 set python_script=%selfdir%resampler.py
-@REM set model_dir=%selfdir%models\usfGAN_EnunuKodoku_0826\
-set model_dir=%selfdir%models\usfGAN_Namineritsu_4130\
-%python_exe% %python_script% %* --model_dir %model_dir% --use_vocoder_model
+set model_dir=%selfdir%models\usfGAN_EnunuKodoku_0826\
+@REM set model_dir=%selfdir%models\usfGAN_Namineritsu_4130\
+%python_exe% %python_script% %* --model_dir %model_dir% --use_vocoder_model --debug
 echo =============================================================================
+@REM timeout /t 1
