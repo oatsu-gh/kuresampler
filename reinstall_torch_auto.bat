81a8b1a296f902e8d4a0649f42c17f15d9ad2019
oatsu <oatsu.dev@gmail.com>
2025-10-26 21:09:31 +0900

    rmdir を for ループ処理

--- reinstall_torch_auto.bat
+++ reinstall_torch_auto.bat
@@ -6,15 +6,9 @@ set python_dir=%~dp0python-3.12.10-embed-amd64\
 @REM Uninstall existing torch packages
 %python_dir%python.exe -m pip uninstall torch torchaudio torchvision -y
 @REM Remove torch directories
-rmdir /s /q %python_dir%Lib\site-packages\torch 2>NUL
-rmdir /s /q %python_dir%Lib\site-packages\torchaudio 2>NUL
-rmdir /s /q %python_dir%Lib\site-packages\torchvision 2>NUL
-rmdir /s /q %python_dir%Lib\site-packages\~orch 2>NUL
-rmdir /s /q %python_dir%Lib\site-packages\~orchaudio 2>NUL
-rmdir /s /q %python_dir%Lib\site-packages\~orchvision 2>NUL
-rmdir /s /q %python_dir%Lib\site-packages\~~orch 2>NUL
-rmdir /s /q %python_dir%Lib\site-packages\~~orchaudio 2>NUL
-rmdir /s /q %python_dir%Lib\site-packages\~~orchvision 2>NUL
+for /d %%D in ("%python_dir%Lib\site-packages\?orch") do rmdir /s /q "%%D" 2>NUL
+for /d %%D in ("%python_dir%Lib\site-packages\?orchaudio") do rmdir /s /q "%%D" 2>NUL
+for /d %%D in ("%python_dir%Lib\site-packages\?orchvision") do rmdir /s /q "%%D" 2>NUL
 
 @REM Upgrade light-the-torch and install torch packages
 %python_dir%python.exe -m pip install --upgrade light-the-torch --no-warn-script-location --disable-pip-version-check
